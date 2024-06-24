6890efcc373e58e5eb707d7403613ac711d4a6f1
leandre <tuyambazeleandre@gmail.com>
2024-06-24 08:46:17 +0200

    add kafla configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from 'express';
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['kafka:9092'],
+});
 
 const app = express();
 
+const producer = kafka.producer();
+
+producer.connect();
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Application works!');
+});
+
+app.get('/send', async (req: Request, res: Response) => {
+  await producer.send({
+    topic: 'topic-test-1', // topic name
+    messages: [{ value: 'Hello KafkaJS user!' + Math.random().toString() }],
+  });
+
+  res.send('Message sent!');
 });
 
 app.listen(3000, () => {
