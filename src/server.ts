11f65d0f60eaed7b1b234fd9ff0524943423e65b
leandre <tuyambazeleandre@gmail.com>
2024-06-24 08:11:49 +0200

    Project setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Application works!');
+});
+
+app.listen(3000, () => {
+  console.log('Application started on port 3000!');
+});
